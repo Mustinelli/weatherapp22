78f1593969256176e916dce6b7e50931c8e76182
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2022-06-07 07:50:57 +0100

    Added testing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import "../styles/App.css";
 import React from "react";
 import Proptypes from "prop-types";
-import "../components/LocationDetails";
+import LocationDetails from "./LocationDetails";
 
 function App() {
   function App(props) {
     const {location} = props;
-  }
-  return (
-  <div className="App">
-    <h1>Weather app</h1>
-    <LocationDetails city = {location.city} country = {location.country}/>
+    return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <LocationDetails city = {location.city} country = {location.country}/>
     </div>
-  );
-}
+  )
 
-export default App;
+
 App.propTypes = {
   location: Proptypes.shape({
     city: Proptypes.string,
     country: Proptypes.string,
   }).isRequired
 };
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../components/App';
 
+describe('App', () => {
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App location = { forecast.location } />);
+  const h1Element = screen.getByText(/Manchester, UK/i);
+  expect(h1Element).toBeInTheDocument();
+});
 });
