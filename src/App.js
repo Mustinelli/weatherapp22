d4e943294b6b50f5c3db683421f6c0c7e88af6a9
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2022-06-07 05:37:34 +0100

    git set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "../styles/App.css";
+import React from "react";
+
+function App() {
+  return (
+  <div className="App">
+    <h1>Weather app</h1>
+    </div>
+  );
+}
+
+export default App;
